f9926a6ac467607bdb7f5fc309a3ba2d8599badd
Ваше Имя <you@example.com>
2017-11-06 14:38:51 +0300

    mainPage + begin of registration page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,10 @@ import { MainPageComponent } from './main-page/main-page.component';
 import { NewsComponent } from './news/news.component';
 import { PageInfoComponent } from './main-page/page-info/page-info.component';
 import { NextNewsComponent } from './main-page/next-news/next-news.component';
+import { TextInfoComponent } from './main-page/page-info/text-info/text-info.component';
+import { RegistrationPageComponent } from './registration-page/registration-page.component';
+import { SignInFormComponent } from './registration-page/sign-in-form/sign-in-form.component';
+import { RegistrationFormComponent } from './registration-page/registration-form/registration-form.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +21,11 @@ import { NextNewsComponent } from './main-page/next-news/next-news.component';
     MainPageComponent,
     NewsComponent,
     PageInfoComponent,
-    NextNewsComponent
+    NextNewsComponent,
+    TextInfoComponent,
+    RegistrationPageComponent,
+    SignInFormComponent,
+    RegistrationFormComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/main-page/page-info/text-info/text-info.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-text-info',
+  templateUrl: './text-info.component.html',
+  styleUrls: ['./text-info.component.scss']
+})
+export class TextInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
