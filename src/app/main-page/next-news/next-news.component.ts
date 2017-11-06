dde69e07473ef9c24932071051d24dc118140c59
Ваше Имя <you@example.com>
2017-11-06 18:48:27 +0300

    end of registration page

--- src/app/main-page/next-news/next-news.component.ts
+++ src/app/main-page/next-news/next-news.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-next-news',
   templateUrl: './next-news.component.html',
-  styleUrls: ['./next-news.component.css']
+  styleUrls: ['./next-news.component.scss']
 })
 export class NextNewsComponent implements OnInit {
 
